c19f98df52285e13b0687038f149f9c0ab3acb39
Pinghai Yuan <pinghaiyuan@gmail.com>
2018-09-03 09:57:30 +0800

    Add unit-test for testing the register allocation algorithm of BaselineCompiler  Changes to be committed: 	modified:   js/src/regrand/regalloc_test.cpp

--- js/src/regrand/regalloc_test.cpp
+++ js/src/regrand/regalloc_test.cpp
@@ -31,23 +31,108 @@
 #include "vm/Interpreter-inl.h"
 #include "vm/NativeObject-inl.h"
 
-int main(int argc, char* argv[])
+using namespace js;
+using namespace js::jit;
+
+
+static JSClassOps global_ops = {
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+    JS_GlobalObjectTraceHook
+};
+
+/* The class of the global object. */
+static JSClass global_class = {
+    "global",
+    JSCLASS_GLOBAL_FLAGS,
+    &global_ops
+};
+
+
+void test_regalloc(void)
 {
 
 
-//Observe AllocatableRegisterSet::takeAnyGeneral()
-    // AllocatableRegisterSet hackregs(RegisterSet::Volatile());
-    // Register hackreg;
-    // for (int i = 0; i< 16; i++) {
-    //     hackreg = hackregs.takeAnyGeneral();
-    //     YPHPRINT("Hack AllocatableRegisterSet::takeAnyGeneral: %ith get %s", i, hackreg.name());
-    // }
 
-//Observe MacroAssebmbler::loadTraceLogger
-//Register loggerReg = regs.takeAnyGeneral();
-//masm.loadTraceLogger(loggerReg);
+    //Observe AllocatableRegisterSet::takeAnyGeneral()
+    AllocatableRegisterSet hackregs(RegisterSet::Volatile());
+    Register hackreg;
+    for (int i = 0; i< 8; i++) {
+        hackreg = hackregs.takeAnyGeneral();
+        printf("Hack AllocatableRegisterSet::takeAnyGeneral: %ith get %s", i, hackreg.name());
+    }
 
-    js::jit::AllocatableGeneralRegisterSet regs(js::jit::GeneralRegisterSet::All());
-    return 0;
+    AllocatableRegisterSet regs(RegisterSet::Volatile());
+    Register reg;
+
+
+    // Observe MacroAssebmbler::loadTraceLogger
+    MacroAssembler masm;
+    reg = regs.takeAnyGeneral();
+    masm.loadTraceLogger(reg);
 }
 
+
+#include "jsapi.h"
+#include "js/Initialization.h"
+
+
+
+int main(int argc, const char *argv[])
+{
+    // Start the engine.
+    if (!JS_Init())
+        return 1;
+
+    JSContext *cx = JS_NewContext(8L * 1024 * 1024);
+    if (!cx)
+        return 1;
+
+    if (!JS::InitSelfHostedCode(cx))
+        return 1;
+
+    { // Scope for our various stack objects (JSAutoRequest, RootedObject), so they all go
+        // out of scope before we JS_DestroyContext.
+
+        JSAutoRequest ar(cx); // In practice, you would want to exit this any
+        // time you're spinning the event loop
+
+        JS::CompartmentOptions options;
+        JS::RootedObject global(cx, JS_NewGlobalObject(cx, &global_class, nullptr, JS::FireOnNewGlobalHook, options));
+        if (!global)
+            return 1;
+
+        JS::RootedValue rval(cx);
+
+        { // Scope for JSAutoCompartment
+            JSAutoCompartment ac(cx, global);
+            JS_InitStandardClasses(cx, global);
+
+            // const char *script = "'hello'+'world, it is '+new Date()";
+            // const char *filename = "noname";
+            // int lineno = 1;
+            // JS::CompileOptions opts(cx);
+            // opts.setFileAndLine(filename, lineno);
+            // bool ok = JS::Evaluate(cx, opts, script, strlen(script), &rval);
+            // if (!ok)
+            //     return 1;
+
+            test_regalloc();
+        }
+
+        JSString *str = rval.toString();
+        printf("%s\n", JS_EncodeString(cx, str));
+    }
+
+    JS_DestroyContext(cx);
+    JS_ShutDown();
+    return 0;
+}
